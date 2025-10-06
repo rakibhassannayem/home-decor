d9c48736aa5068948c0a898accbe2651669a3597
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-06 16:33:45 +0600

    footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,11 @@ import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <Outlet />
+      <div className="flex-1">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
