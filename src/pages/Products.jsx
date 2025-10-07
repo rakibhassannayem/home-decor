d269b7e9d8c7c69dbea18014c11699aa4a098912
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-07 10:13:00 +0600

    search functionality

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,35 @@
 import useProducts from "../hooks/useProducts";
-import { Search } from "lucide-react";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
 
 const Products = () => {
   const { products } = useProducts();
+  const [search, setSearch] = useState("");
+  const term = search.trim().toLowerCase();
+  const searchedProducts = term
+    ? products.filter((product) => product.name.toLowerCase().includes(term))
+    : products;
+
   return (
     <div>
       <div className="flex items-center justify-between mb-4">
-        <h1 className="text-2xl font-bold">All Products</h1>
-        <button className="flex items-center gap-1 text-lg hover:scale-105 transition ease-in-out cursor-pointer">
-          Search <Search size={16} />
-        </button>
+        <h1 className="text-2xl font-bold">
+          All Products{" "}
+          <span className="text-sm text-gray-500 font-medium">
+            ({searchedProducts.length}) products found
+          </span>
+        </h1>
+        <label className="input">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="Search Products"
+          />
+        </label>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
+        {searchedProducts.map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
